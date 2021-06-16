2f3a7851f4a35abdaadc43d5c3f801cf09bcb477
trubadger <social@trubadger.io>
2021-06-16 20:30:11 +0300

    bugfix

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,5 @@
 var trbgr = artifacts.require('./TRBGR');
 var CataBoltSwap = artifacts.require('./CataBoltSwap');
 module.exports = function(deployer) {
-  //deployer.deploy(trbgr);
   deployer.deploy(CataBoltSwap);
 };
